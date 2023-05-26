915cd93d5b5208becb6ed6e3aa8d1151c45f3f4a
Dave Bulaquena <davecharm16@gmail.com>
2023-05-26 13:19:10 +0800

    Added Data Grid to Dashboard

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,20 +1,106 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React, {useEffect, useState} from 'react'
 import Nav from '../components/Nav'
+import { DataGrid, GridToolbar  } from '@mui/x-data-grid';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from '../utils/utils';
+import { getDatabase, ref, onValue} from "firebase/database";
+import moment from 'moment/moment';
 
 const Admin = () => {
 
-  const [data, setData]  = useState();
-  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const [data, setData]  = useState([]);
+  const db = getDatabase(app);
+
+  const columns = [
+    { field: 'UID', headerName: 'CARD ID', width: 90 },
+    {
+      field: 'balance',
+      headerName: 'Load Balance',
+      width: 150,
+      valueGetter : (params) => `₱ ${parseFloat(params.row.balance).toFixed(2)}`
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 150,
+    },
+    {
+      field: 'plateNumber',
+      headerName: 'Plate Number',
+      width: 110,
+    },
+    {
+      field: 'entryDateTime',
+      headerName: 'Entry Date Time',
+      width: 200,
+      valueGetter: (params) =>
+        (params.row.entryDateTime != "") ? moment(params.row.entryDateTime).format("MMMM DD YYYY, h:mm:ss a") : '',
+    },
+    {
+      field: 'exitDateTime',
+      headerName: 'Exit Date Time',
+      sortable: false,
+      width: 200,
+      valueGetter: (params) =>{
+        if(params.row.exitDateTime == ''){
+          return ""
+        }
+        else{
+          return moment(params.row.exitDateTime).format("MMMM DD YYYY, h:mm:ss a")
+        }
+      },
+    },
+    {
+
+    },
+  ];
+
   useEffect(() => {
-    
+
+    const starCountRef = ref(db, 'users/');
+    onValue(starCountRef, (snapshot) => {
+      const d = snapshot.val();
+      console.log(d);
+      let d_array = []
+      for (const key in d) {
+        d_array.push(d[key]);
+      }
+      setData(d_array);
+    });
   }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  const getRowId = (row) => row.UID; 
 
   return (
     <Box>
       <Nav/>
-      <Box p='20px' width='100vw' height='100vh'>
-        
+      <Box p='20px' width='100vw'>
+        <Typography variant='h2' sx ={{
+          textAlign:'center',
+          mb : '20px'
+        }}> LIST OF CARDS</Typography>
+        <Box maxWidth='1100px' width='80vw' mx='auto'>
+
+          <DataGrid columns={columns} rows = {data} getRowId={getRowId}
+           initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                },
+              },
+            }}
+            pageSizeOptions={[5]}
+            slots={{ toolbar: GridToolbar }}
+          />
+        </Box>
       </Box>
     </Box>
   )
