93d609987d99c7bf49ef203d67289689a72094db
Dave Bulaquena <davecharm16@gmail.com>
2023-05-30 19:15:03 +0800

    Added Some UI on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,11 @@ const Home = () => {
 
   const [card, setCard] = useState(null);
   const [cardId , setCardId] = useState('');
+  const [payment, setPayment] = useState('');
+  const [search, setSearch] = useState(false);
 
   const handleCard = () => {
+    setSearch(true);
     console.log(cardId);
     const starCountRef = ref(db, `users/${cardId}`);
     onValue(starCountRef, (snapshot) => {
@@ -31,10 +34,10 @@ const Home = () => {
       }}
     >
       <Typography variant='h1' color={theme.palette.primary.main} fontWeight='bold'>RFID PARKING</Typography>
-      <Typography variant='h2' color={theme.palette.secondary.main} fontWeight='bold' >Load your Card Here</Typography>
+      <Typography variant='h2' color={theme.palette.secondary.main} fontWeight='bold' >Load your card here</Typography>
       <Box display='flex' justifyContent='center' alignItems='center'>
         <TextField type='text' name='card_name' variant='outlined' label='CARD NUMBER' 
-          sx = {{m: '20px', 
+          sx = {{m: '10px', 
           }}
           onChange={(e)=>{
             setCardId(e.target.value);
@@ -49,22 +52,46 @@ const Home = () => {
         <Card sx = {{width : '300px'}}>
           <CardContent sx= {{display:'flex', justifyContent:'center'}}>
             <Typography variant='h6' color={theme.palette.primary.main} fontWeight='bold' mx='10px' sx = {{flex : 1}}>Card Number</Typography>
-            <Typography variant='h6' color={theme.palette.secondary.main}>04 2F EG 1C</Typography>
+            <Typography variant='h6' color={theme.palette.secondary.main}>{card.UID}</Typography>
           </CardContent>
           <CardContent sx= {{display:'flex', justifyContent:'center'}}>
             <Typography variant='h6' color={theme.palette.primary.main} fontWeight='bold' mx='10px' sx = {{flex : 1}}>Card Holder</Typography>
-            <Typography variant='h6' color={theme.palette.secondary.main}>Jane Doe</Typography>
+            <Typography variant='h6' color={theme.palette.secondary.main}>{card.name}</Typography>
           </CardContent>
           <CardContent sx= {{display:'flex', justifyContent:'center'}}>
             <Typography variant='h6' color={theme.palette.primary.main} fontWeight='bold' mx='10px' sx = {{flex : 1}}>Card Balance</Typography>
-            <Typography variant='h6' color={theme.palette.secondary.main}>20.00</Typography>
+            <Typography variant='h6' color={theme.palette.secondary.main}>₱ {parseFloat(card.balance).toFixed(2)}</Typography>
           </CardContent>
         </Card>
-        <Typography my='20px'>Select Payment Method</Typography>
+        <Typography my='20px' fontWeight='bold'>Select Payment Method</Typography>
         <Box>
-          
+              <Box className="cc-selector" display='flex' justifyContent='center' alignItems='center' >
+                <Box mx='10px'>
+                  <input id="gcash" type="radio" name="payment" value="gcash" onChange={(e)=>{
+                    setPayment(e.target.value)
+                  }}/>
+                  <label className="drinkcard-cc gcash" htmlFor="gcash"></label>
+                </Box>
+                <Box mx='10px'>
+                  <input id="visa" type="radio" name="payment" value="visa" onChange={(e)=>{
+                    setPayment(e.target.value)
+                  }}/>
+                  <label className="drinkcard-cc visa" htmlFor="visa"></label>
+                </Box>
+                <Box mx='10px'>
+                  <input id="mastercard" type="radio" name="payment" value="mastercard" onChange={(e)=>{
+                    setPayment(e.target.value)
+                  }} />
+                  <label className="drinkcard-cc mastercard" htmlFor="mastercard"></label>
+                </Box>
+              </Box>
+              {payment}
         </Box>
         </>
+      }
+
+      {
+        (card === null && search === true) && <Typography variant='h2' color='#DF4554'>Card is Not Registered / Found</Typography> 
       }
         
       </Box>
