009f8c4e4607ad0fc564e1f0c01b985cf5b46006
Dave Bulaquena <davecharm16@gmail.com>
2023-05-31 12:57:01 +0800

    Added Payment Method UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const Card = () => {
   return (
-    <div>
+    <Box>
       
-    </div>
+    </Box>
   )
 }
 
--- src/components/PayMaya.jsx
+++ src/components/PayMaya.jsx
@@ -1,10 +1,154 @@
-import React from 'react'
+import { Box, Button, TextField, Typography, Alert } from '@mui/material'
+import React, {useState} from 'react'
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import { firebaseConfig } from '../utils/utils';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, set, push } from 'firebase/database'
+import moment from 'moment';
 
-const PayMaya = () => {
+
+
+const PayMaya = ({cardId, balance}) => {
+
+  const app = initializeApp(firebaseConfig);
+
+  const [mobileNum , setMobileNum] = useState('');
+  const [loadVal, setLoadVal] = useState('');
+  const [errorOpen, setErrorOpen] = useState(false);
+  const [error, setError] = useState('Error : ');
+  
+
+  const handleLoad = ()=>{
+    setErrorOpen(false);
+    let pattern = /^(09|\+639)\d{9}$/
+    if(!pattern.test(mobileNum)){
+      setError('Invalid Mobile Number, Please Enter a Valid Mobile Number');
+      setErrorOpen(true);
+    }
+
+    if(loadVal === '' || loadVal === 0) {
+      setError('Please Enter A Valid Amount Greater than Zero!');
+      setErrorOpen(true);
+    }
+    else{
+      if(!errorOpen){
+        console.log('Load Here')
+        const newBalance = parseFloat(loadVal) + parseFloat(balance);
+        const database = getDatabase(app);
+        const valueRef = ref(database, `users/${cardId}/balance`);
+        
+        set(valueRef, newBalance)
+        .then(() => {
+            updateTopUpData();
+            console.log('Value updated successfully!');
+        })
+        .catch((error) => {
+            alert(error);
+            console.error('Error updating value:', error);
+        });
+      }
+    }
+  }
+
+  
+  const updateTopUpData = () =>{
+    const database = getDatabase();
+    const dataRef = ref(database, `users/${cardId}/topup`);
+    const topUpRef = ref(database, `topups/`);
+
+    const newTopUpRef = push(topUpRef);
+    const newDataRef = push(dataRef);
+
+    const newTopUpData = {
+        card_id : cardId,
+        date : moment().format(),
+        load_value : parseInt(loadVal)
+    }
+
+    const newData = {
+        date : moment().format(),
+        value : parseInt(loadVal)
+    }
+
+    set(newDataRef,newData)
+    .then(() => {
+        console.log('New data added successfully! Load Top up');
+    })
+    .catch((error) => {
+        console.error('Error adding new data:', error);
+    });
+
+    set(newTopUpRef,newTopUpData)
+    .then(() => {
+        console.log('New data added successfully!');
+        setLoadVal(0);
+        alert('Load successful');
+    })
+    .catch((error) => {
+        console.error('Error adding new data:', error);
+    });
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <Collapse in={errorOpen}>
+      <Alert severity="error"
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setErrorOpen(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+        sx={{ mb: 2 }}
+      >
+        {error}
+      </Alert>
+    </Collapse>
+    <Box
+      sx = {{
+        // border : '1px solid #000',
+        display : 'flex',
+        padding : '5px',
+        alignItems : 'center',
+      }}
+    >
+      <Box display='flex' alignItems='center'>
+        <Typography variant='h5' mx='10px' color = '#02B566' fontWeight='bold'>PayMaya Details</Typography>
+        <TextField label='Your PayMaya No.' type='text' sx = {{
+          '& .MuiInputLabel-root'  : {
+            fontWeight : 'bold',
+            color : '#02B566',
+          }
+        }}
+          value= {mobileNum}
+          onChange={(e)=>{setMobileNum(e.target.value)}}
+        />
+      </Box>
+      <Box display='flex' alignItems='center'>
+        <Typography variant='h5' mx='10px' color = 'secondary' fontWeight='bold'>Load</Typography>
+        <TextField label='Enter Amount' type='text' sx = {{
+          '& .MuiInputLabel-root'  : {
+            fontWeight : 'bold',
+            color : '#025464',
+          }
+        }}
+        value={loadVal}
+        onChange={(e)=>{setLoadVal(e.target.value)}}
+        />
+      </Box>
+      <Button variant='contained'  color='success' sx = {{mx: '10px'}} onClick = {handleLoad}>
+        Load Card
+      </Button>
+    </Box>
+    </>
   )
 }
 
